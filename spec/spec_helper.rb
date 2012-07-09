cd8ab7a2564df619ace6424e4b35d324f98ad21b
Ben Woodall <mail@benwoodall.com>
2012-07-09 07:27:25 -0700

    mocks no longer work, need to be updated

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,12 @@
 $TESTING=true
 $:.push File.join(File.dirname(__FILE__), '..', 'lib')
 
-require 'brewerydb'
+require 'brewerydb2'
 
 def mock_httparty_response(parsed_response)
   request_object = HTTParty::Request.new Net::HTTP::Get, '/'
   response_object = Net::HTTPOK.new('1.1', 200, 'OK')
   response_object.stub(:body => "{foo:'bar'}")
 
-  HTTParty::Response.new(request_object, response_object, parsed_response) 
+  HTTParty::Response.new(request_object, response_object, parsed_response)
 end
